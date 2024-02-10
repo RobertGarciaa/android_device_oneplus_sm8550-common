8af04bc99c655b29da19a8939f9d65f3640d1442
RobertGarciaa <chae0218@naver.com>
2024-02-10 18:34:37 +0900

    sm8550-common: Add missing VINTF manifest
    Log:
    01-22 08:18:10.039   688   688 I servicemanager: Could not find android.hardware.bluetooth.IBluetoothHci/default in the VINTF manifest.
    01-22 08:16:50.059   688   688 I servicemanager: Could not find android.hardware.soundtrigger3.ISoundTriggerHw/default in the VINTF manifest.
    01-22 08:16:50.066   688   688 I servicemanager: Could not find android.hardware.thermal.IThermal/default in the VINTF manifest.
    01-22 08:16:50.067   688   688 I servicemanager: Could not find android.hardware.usb.gadget.IUsbGadget/default in the VINTF manifest.
    01-22 08:16:49.903   688   688 I servicemanager: Could not find android.hardware.oemlock.IOemLock/default in the VINTF manifest.
    01-22 08:16:29.202   688   688 I servicemanager: Could not find android.hardware.vibrator.IVibratorManager/default in the VINTF manifest.
    01-22 08:18:26.096   688   688 I servicemanager: Could not find android.hardware.dumpstate.IDumpstateDevice/default in the VINTF manifest.
    
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- common.mk
+++ common.mk
@@ -409,7 +409,8 @@ PRODUCT_COPY_FILES += \
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/oplus/vintf/device_framework_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    device/oneplus/sm8550-common/framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += device/oneplus/sm8550-common/vintf/framework_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := \
