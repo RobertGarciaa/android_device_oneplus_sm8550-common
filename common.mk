a967c8e9539452a9e0ebe65951b86233c9c18ff1
Sugakesshaa <sugakesshaa@pixelos.net>
2024-06-10 19:43:45 +0530

    sm8550-common: Move init.qcom.post_boot.sh to tree
    Change-Id: Ia442e542c8f16330acef59156029034e323cf8ad
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- common.mk
+++ common.mk
@@ -188,6 +188,7 @@ PRODUCT_PACKAGES += \
     init.kernel.post_boot-kalama.sh \
     init.oplus.rc \
     init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.recovery.rc \
     init.qcom.sh \
