972d72f310f269c34e75ad6bbe3b153e9620a7fa
Bruno Martins <bgcngm@gmail.com>
2024-06-10 19:51:18 +0530

    sm8550-common: Migrate to common libqti-perfd-client and power-libperfmgr
    Change-Id: Ic7d8453af8da9867bef79b4715a853024df4b230
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- common.mk
+++ common.mk
@@ -283,11 +283,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client \
     liboplus-uah-client
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/kalama/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QSPA
 PRODUCT_PACKAGES += \
@@ -325,8 +323,12 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/oplus \
-    hardware/pixelworks/interfaces
+    hardware/qcom-caf/common/libqti-perfd-client \
+	hardware/pixelworks/interfaces
 
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
