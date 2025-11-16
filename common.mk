28fd450baadfde4e30f9084d7cf9c95c6abd008d
LuK1337 <priv.luk@gmail.com>
2025-11-16 10:56:33 +0100

    sm8550-common: Opt out of speaker_layout_channel_mask field
    Required for ABI compatibility with audio HAL compiled before 15 QPR2.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
    Change-Id: Ic910f0634ee0707c21dad77f5264fd89fb33f1c0

--- common.mk
+++ common.mk
@@ -85,6 +85,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.dynamic.head_tracker.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.dynamic.head_tracker.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     lib_bt_aptx \
