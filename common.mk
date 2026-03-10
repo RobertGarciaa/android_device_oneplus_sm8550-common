3e8991adaaef42619cf358334530717ad13ff38f
LuK1337 <priv.luk@gmail.com>
2026-03-10 09:12:28 +0100

    sm8550-common: Remove fwk-detect lib from PRODUCT_PACKAGES
    See: Iac75d83e6bc46c137c328a2365e6701c210bfe7a
    Change-Id: I6b8571e81e59f438e34b76823f47ff024ec33dc6

--- common.mk
+++ common.mk
@@ -296,10 +296,6 @@ PRODUCT_PACKAGES += \
     qspa_vendor.rc \
     vendor.qti.qspa-service
 
-# QTI fwk-detect
-PRODUCT_PACKAGES += \
-    libvndfwk_detect_jni.qti_vendor # Needed by CNE app
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
